a6bbfbedd2bf530879acc2534663e64f4765639f
Steve Burtenshaw <53346+stphnnnn@users.noreply.github.com>
2021-08-22 14:55:36 +0100

    remove tabletop to fix csv parsing

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,19 +1,29 @@
-import tabletop from "tabletop";
+import Papa from "papaparse";
+import { get } from "axios";
 import { getStudios, getLocations } from "../../dataHelpers";
 
 const SPREADSHEET_URL =
-  "https://docs.google.com/spreadsheets/d/1_FirimvkCUlc1VOiNI8tuDfXGWWQW9BDQ_2xq5qgJ2E/edit?usp=sharing";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSrmesVzQkYTyU3DNuziD7C-h8hYmC9SGpiDDa9Em_tm0WCkobF1rZWjR4BW15hu9QALeEgfIofSeI-/pub?output=csv";
 
 export default async (req, res) => {
-  const data = await tabletop.init({
-    key: SPREADSHEET_URL,
-    simpleSheet: true,
-  });
+  try {
+    const { data } = await get(SPREADSHEET_URL);
+    Papa.parse(data, {
+      header: true,
+      skipEmptyLines: true,
+      complete: async (results) => {
+        const data = results.data;
+        const studios = getStudios(data);
+        const locations = getLocations(studios);
 
-  const studios = getStudios(data);
-  const locations = getLocations(studios);
-
-  res.setHeader("Content-Type", "application/json");
-  res.statusCode = 200;
-  res.end(JSON.stringify({ studios, locations }));
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ studios, locations }));
+      },
+    });
+  } catch {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ message: "Something went wrong" }));
+  }
 };
